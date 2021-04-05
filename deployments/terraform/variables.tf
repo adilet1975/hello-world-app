59ae72d8a20f2c29d8992d15205139a421f427b3
adilet1975 <74070729+adilet1975@users.noreply.github.com>
2021-04-04 23:30:32 -0500

    Update variables.tf

--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -1,12 +1,13 @@
 variable  "deployment_image" {
-    default = "docker.fuchicorp.com/hello-world-app-dev-feature:4b7aacb"
+    default = "docker.fuchicorp.com/hello-world-stage:c6e96b5"
 }
 variable "deployment_environment" {
     default = "dev"
 }
 variable "deployment_endpoint" {
     type = "map"
-     default = {
+    
+    default = {
         test  = "test.hello"
         dev  = "dev.hello"
         qa   = "qa.hello"
